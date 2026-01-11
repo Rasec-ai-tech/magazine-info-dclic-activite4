6b1294af24f7abf4a98bd7ae6c9adb80bf231390
Rasec-ai-tech <jeanimien@gmail.com>
2026-01-11 21:08:43 +0000

    Commit initial : ajout du projet

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MonAppli());
+}
+
+class MonAppli extends StatelessWidget {
+  const MonAppli({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Magazine',
+      debugShowCheckedModeBanner: false,
+      home: const pageAccueil(),
+    );
+  }
+}
+
+class pageAccueil extends StatelessWidget {
+  const pageAccueil({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Magazine Infos'),
+        centerTitle: true,
+        backgroundColor: Colors.pink,
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/magazineInfo.jpg'),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Tu as cliqué dessus')),
+          );
+        },
+        backgroundColor: Colors.pink,
+        child: const Text('Click'),
+      ),
+    );
+  }
+}
+
